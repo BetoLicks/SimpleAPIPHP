da114a5e478427e6561bd49904a7bd0e46bca36f
Beto Licks <betaolicks@gmail.com>
2023-10-16 09:51:30 -0300

    fix: refatora clientes

--- clientes/clientes.php
+++ clientes/clientes.php
@@ -2,12 +2,51 @@
 
 if ($api == 'clientes'){
    if ($metodo == 'GET'){
-      $db = DB::conexaoDB();
-      $retorno = $db->prepare("SELECT * FROM tab_clientes ORDER BY cli_nome");
-      $retorno->execute();
-      $linhas = $retorno->fetchAll(PDO::FETCH_ASSOC);
+      //-> ERRO DE ROTA...
+      if ($acao == ''){
+         echo json_encode(["ERRO"=>'Rota inexistente']);
+      }
 
-      var_dump($linhas);
+      //-> TENTA A CONEXÃO COM O BANCO...
+      $db  = DB::conexaoDB();
+      $sql = '';
+      $act = '';
+
+      //-> LISTAGEM DE TODOS OS CLIENTES...
+      if (($acao == 'lista') && ($param == '')){
+         $sql = 'SELECT * FROM tab_clientes ORDER BY cli_nome';         
+         $act = 'listAll';
+      }
+
+      //-> RETORNA UM ÚNICO CLIENTE...
+      if (($acao == 'lista') && ($param != '')){
+         $sql = "SELECT * FROM tab_clientes WHERE cli_codigo = '$param' ";
+         $act = 'listOne';            
+      }
+
+      $retorno = $db->prepare($sql);
+      $retorno->execute();
+
+      //-> AÇÃO A SER TOMADA...
+      switch ($act) {
+         case 'listAll':
+            $linhas = $retorno->fetchAll(PDO::FETCH_ASSOC);
+            break;
+         case 'listOne':
+            $linhas = $retorno->fetchObject();
+            break;
+     }      
+
+      if ($linhas){
+         echo json_encode(["dadosCliente"=>$linhas]);
+      } else {
+         echo json_encode(["AVISO"=>'Nenhum cliente encontrado']);
+      }         
+
+
+
+
+
 
    }
 }
